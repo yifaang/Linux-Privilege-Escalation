981c2b0b55a7858f7d260c3603a25b6593126c57
yifaang <yifangtongxing48@gmail.com>
2022-04-15 08:09:52 -0400

    zip the codes

--- main.py
+++ main.py
@@ -1,133 +1,74 @@
 #!/usr/bin/env python3
 #-*- codeing:utf-8 -*-
-import subprocess,json,re,time,sys,requests
+import subprocess,json,re,time,sys,requests,random
 from colorama import init,Fore
 from optparse import OptionParser
 init(autoreset=True)
-
-class kernel:                                                               #初始化获得操作系统信息
-    # linuxinfo_dic={'Kernel Version':'',
-    #                'Sudo location':'',
-    #                'uname location':'',
-    #                'lsb_release':''}
+class kernel_linux:
     def __init__(self):
-        self.cve_dic = self.import_json()
-        self.linuxinfo_dic={'Kernel Version':'',                            #输出到一个字典当中
-                            'sudo':'',
-                            'sudo Verison':'',
-                            'uname':'',
-                            'lsb_release':'',}
+        self.cve_dic = self.import_json()              #读取exploit.json文件
+        self.linuxinfo_dic = {'Kernel Version': '',   #初始化字典
+                              'sudo': '',
+                              'sudo Verison': '',
+                              'uname': '',
+                              'lsb_release': '', }
         kernel_Version = subprocess.Popen('uname -r', shell=True, stdout=subprocess.PIPE)
-        sudo = subprocess.Popen('which sudo',shell=True,stdout=subprocess.PIPE)
+        sudo = subprocess.Popen('which sudo', shell=True, stdout=subprocess.PIPE)
         sudo_version = subprocess.Popen('sudo -V', shell=True, stdout=subprocess.PIPE)
-        uname = subprocess.Popen('uname -a',shell=True,stdout=subprocess.PIPE)
-        lsb_release = subprocess.Popen('lsb_release -a',shell=True,stdout=subprocess.PIPE)
-        self.linuxinfo_dic['Kernel Version'] = bytes.decode(kernel_Version.stdout.read()).replace('\n','')
-        self.linuxinfo_dic['sudo'] =  bytes.decode(sudo.stdout.read()).replace('\n','')
-        self.linuxinfo_dic['sudo Version'] = bytes.decode(sudo_version.stdout.read()).replace('\n','\n\t')
+        uname = subprocess.Popen('uname -a', shell=True, stdout=subprocess.PIPE)
+        lsb_release = subprocess.Popen('lsb_release -a', shell=True, stdout=subprocess.PIPE)
+        self.linuxinfo_dic['Kernel Version'] = bytes.decode(kernel_Version.stdout.read()).replace('\n', '')
+        self.linuxinfo_dic['sudo'] = bytes.decode(sudo.stdout.read()).replace('\n', '')
+        self.linuxinfo_dic['sudo Version'] = bytes.decode(sudo_version.stdout.read()).replace('\n', '\n\t')
         self.linuxinfo_dic['uname'] = bytes.decode(uname.stdout.read())
-        self.linuxinfo_dic['lsb_release'] = bytes.decode(lsb_release.stdout.read()).replace('\n','\n\t')
+        self.linuxinfo_dic['lsb_release'] = bytes.decode(lsb_release.stdout.read()).replace('\n', '\n\t')
 
-    def systeminfo(self):                                                   #工具启动时进行当前版本信息显示
-        print(Fore.BLUE+'[!][Linux System Information]')
-        print('[+]The Sytem Kernel Version is \n\t' + Fore.GREEN+self.linuxinfo_dic['Kernel Version'])
-        print('[+]The Sudo is in: \n\t' + Fore.GREEN+self.linuxinfo_dic['sudo'])
-        print('[+]SUDO Version: \n\t' + Fore.GREEN+self.linuxinfo_dic['sudo Version'])
-        print('[+]System name: \n\t' + Fore.GREEN+self.linuxinfo_dic['uname'])
-        print('[+]GNU/Linux Info: \n\t'+Fore.GREEN+self.linuxinfo_dic['lsb_release'])
-        print(Fore.CYAN+'-----------------------------------------------------------------------------------------------------------')
-
-    def ckeck_CVE(self):                                                    #检测CVE
-        #type 存在 sudo samba kernel
-        version = self.linuxinfo_dic['Kernel Version']
-        temp_list = re.findall('(\d)\.(.*?)\.(.*?)-',version)
-        one,two,three = temp_list[0]
-        # for type in self.cve_dic['Kernel Linux']:
-        #     print(type)
-        self.kernel_CVE_Check(one,two,three)#检查操作系统内核漏洞
-        # print(one,two,three)
-        #开始循环 便利
-        
     def import_json(self):                                                  #导入CVE.json文件
         with open('./exploit.json','r') as f:
             cve_dic = json.loads(f.read())
-            return cve_dic
+            return cve_dic          #返回列表数据
 
-    def Github_CVE(self,query):
-        url = 'https://api.github.com/search/repositories?q=' + query + '&sort=updated'
-        data = requests.get(url).content.decode('utf-8')
-        # print(data)
-        datalist = json.loads(data)
-        print('Find ' + str((datalist['total_count'])) + ' result')
-        for i in range(0, datalist['total_count'] - 1):
-            print(datalist['items'][i]['full_name'])
+    def GetVersion(self,version_tried):
+        origin_list = re.findall('(\d)\.(.*?)\.(.*)',version_tried)
+        a,b,c = origin_list[0]
+        return (a,b,c)
 
-    def kernel_CVE_Check(self,one,two,three):
-        # for cve in self.cve_dic:
+    def systeminfo(self):
+        print(Fore.BLUE + '[!][Linux System Information]')
+        print('[+]The Sytem Kernel Version is \n\t' + Fore.GREEN + self.linuxinfo_dic['Kernel Version'])
+        print('[+]The Sudo is in: \n\t' + Fore.GREEN + self.linuxinfo_dic['sudo'])
+        print('[+]SUDO Version: \n\t' + Fore.GREEN + self.linuxinfo_dic['sudo Version'])
+        print('[+]System name: \n\t' + Fore.GREEN + self.linuxinfo_dic['uname'])
+        print('[+]GNU/Linux Info: \n\t' + Fore.GREEN + self.linuxinfo_dic['lsb_release'])
+        print(Fore.CYAN + '-----------------------------------------------------------------------------------------------------------------------')
+
+    def CVE_check(self):
         print(Fore.BLUE + 'Start the Kernel Linux CVE Check .....')
-        for type in self.cve_dic['Kernel Linux']:
-            i = 0
-            m = 0
-            z = 0
-            temp_version_list = self.cve_dic['Kernel Linux'][type]['Version']
-            for version in temp_version_list:
-                # time.sleep(0.1)
-                # print(type)
-                if '->' not in version:
-                    if ('>' not in version) and ('<' not in version):
-                        a,b,c = self.GetVersion('kernel',version)
-                        if a == one:
-                            if (b == two):
-                                if (c == three) or (c == '*'):
-                                    z+=1
-                            elif (b == '*'):
-                                z+=1
-                            else:
-                                pass
-                        else:
-                            pass
-                    else:
-                        if '>' in version:
-                            l = re.findall('>(\d)\.(.*?)\.', version)
-                            m_a_1, m_a_2 = l[0]
-                            if (float(str(one)+'.'+str(two)) >= float(str(m_a_1)+'.'+str(m_a_2))):
-                                z+=1
-                            else:
-                                pass
-                        elif '<' in version:
-                            lb = re.findall('<(\d)\.(.*?)\.', version)
-                            m_b_1, m_b_2 = lb[0]
-                            if (float(str(one)+'.'+str(two)) <= float(str(m_b_1)+'.'+str(m_b_2))):
-                                z+=1
-                            else:
-                                pass
-                else:
-                    b = re.findall('(\d)\.(.*?)\.', version)
-                    s_a_1,s_a_2 = b[0]
-                    s_b_1,s_b_2 = b[1]
-                    if (float(str(one)+'.'+str(two)) >= float(str(s_a_1)+'.'+str(s_a_2))) and (float(str(one)+'.'+str(two)) <= float(str(s_b_1)+'.'+str(s_b_2))):
-                        z+=1
-                    else:
+        temp_data = str(self.linuxinfo_dic['Kernel Version'])
+        origin_list = re.findall('(\d)\.(.*?)\.(.*?)-', temp_data)
+        ver1,ver2,ver3 = origin_list[0]
+        for cve in self.cve_dic['Kernel Linux']:
+            for ver_sion in self.cve_dic['Kernel Linux'][cve]['Version']:
+                try_ver1,try_ver2,try_ver3 = self.GetVersion(ver_sion)
+                while True:
+                    if ver1 == try_ver1:
                         pass
-            if z >0:
-                print(Fore.GREEN+'[!]System hava '+type+'\n ﹂The download is '+(self.cve_dic['Kernel Linux'][type]['Exploition'][0]))
+                    else:
+                        break
+                    if (try_ver2 == '*') or (ver2 == try_ver2):
+                        pass
+                    else:
+                        break
+                    if (try_ver3 == '*') or (ver3 == try_ver3):
+                        print(Fore.GREEN+'[!]System hava '+cve+'\n ﹂The download address is '+(self.cve_dic['Kernel Linux'][cve]['Exploition'][0]))
+                        break
+                    else:
+                        break
 
-    def GetVersion(self,type,origin):
-        origin = str(origin)
-        if type == 'kernel':
-            origin_list = re.findall('(\d)\.(.*?)\.(.*)',origin)
-            a,b,c = origin_list[0]
-            return (a,b,c)
-            # print(b)
-            print(origin_list)
-        elif type =='sudo':
-            print(Fore.BLUE+'Start check the SUDO CVE......')
-        elif type =='samba':
-            print(Fore.BLUE+'Start check the samba CVE......')
-
+class tools:
     def update(self):
         print(Fore.GREEN+"Update the programe!!!")
-        find.cmd('git clone https://github.com/yifaang/Linux-Privilege-Escalation.git /usr/share/Linux-Privilege-Escalation/')
+        self.cmd('git clone https://github.com/yifaang/Linux-Privilege-Escalation.git /usr/share/Linux-Privilege-Escalation/')
 
     def banner(self):
         print(Fore.GREEN+"                                            ")
@@ -139,10 +80,21 @@ class kernel:                                                               #初
         print(Fore.GREEN+"         ║ ║     ║  ║     ╚══╗ ╠═══ ║        ")
         print(Fore.GREEN+"         ╚═╝     ╚══╝     ╚══╝ ╚═══ ╚═══╝    ")
         print(Fore.GREEN+"                                             ")
-        
-class linuxfind:
-    def __init__(self):
-        pass
+
+    def Github_CVE(self,query):
+        url = 'https://api.github.com/search/repositories?q=' + query + '&sort=updated'
+        data = requests.get(url).content.decode('utf-8')
+        datalist = json.loads(data)
+        print('Find ' + str((datalist['total_count'])) + ' result')
+        for i in range(0, datalist['total_count'] - 1):
+            print(datalist['items'][i]['full_name'])
+
+    def cmd(self,cmd):
+        shell = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        shell = bytes.decode(shell.stdout.read()).replace('\n','\n\t')
+        return shell
+
+class other:
     def SUID_Check(self):
         print(Fore.BLUE+'Start find SUID file.....')
         for cmd in linux.cve_dic['Command']['SUID']:
@@ -163,14 +115,10 @@ class linuxfind:
         for cmd in linux.cve_dic['Command']['Service']:
             print(linux.cve_dic['Command']['Service'][cmd][0] + '\n\t' + Fore.GREEN + self.cmd(cmd))
 
-    def cmd(self,cmd):
-        shell = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        shell = bytes.decode(shell.stdout.read()).replace('\n','\n\t')
-        return shell
-
 if __name__ == '__main__':
-    linux = kernel()        #实例化对象
-    find = linuxfind()
+    linux = kernel_linux()        #实例化对象
+    other = other()
+    tools = tools()
     parser = OptionParser("Usage: %prog -h")
     parser.add_option("-V","--version",action="store_true",help="program version")
     parser.add_option("-C","--CVE-check",action="store_true",help="CVE Check")
@@ -192,7 +140,7 @@ if __name__ == '__main__':
         find.logs()
     if options.CVE_check == True:
         linux.systeminfo()
-        linux.ckeck_CVE()
+        linux.CVE_check()
     if options.Git_Query != None:
         linux.Github_CVE(options.Git_Query)
     if options.info == True:
@@ -205,7 +153,4 @@ if __name__ == '__main__':
     if options.update == True:
         linux.update()
     if options.banner == True:
-        linux.banner()
-
-
-
+        tools.banner()
